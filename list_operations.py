92ac92514f70137282124f82b84aa533f3f86aa1
Hackbright Student <user@mini-002.(none)>
2014-02-06 16:13:28 -0800

    part 1 complete and passed

--- list_operations.py
+++ list_operations.py
@@ -20,67 +20,68 @@ test_list_operations.py for concrete examples of the expected function behavior.
 
 def head(input_list):
     """Return the first element of the input list."""
-    pass
+    return input_list[0]
 
 def tail(input_list):
     """Return all elements of the input list except the first."""
-    pass
+    return input_list[1:]
 
 def last(input_list):
     """Return the last element of the input list."""
-    pass
+    return input_list[-1]
 
 def init(input_list):
     """Return all elements of the input list except the last."""
-    pass
+    return input_list[:-1]
 
 def first_three(input_list):
     """Return the first three elements of the input list."""
-    pass
+    return input_list[:3]
 
 def last_five(input_list):
     """Return the last five elements of the input list."""
-    pass
+    return input_list[-5:]
 
 def middle(input_list):
     """Return all elements of the input list except the first two and the last
     two.
     """
-    pass
+    return input_list[2:-2]
 
 def inner_four(input_list):
     """Return the third, fourth, fifth, and sixth elements of the input list."""
-    pass
+    return input_list[2:6]
 
 def inner_four_end(input_list):
     """Return the sixth, fifth, fourth, and third elements from the end of the
-    list, in that order.
-    """
-    pass
+    list, in that order."""
+    return input_list[-6:-2]
 
 def replace_head(input_list):
     """Replace the head of the input list with the value 42."""
-    pass
+    input_list[0] = 42
 
 def replace_third_and_last(input_list):
     """Replace the third and last elements of the input list with the value 37."""
-    pass
+    input_list[2] = 37
+    input_list[-1] = 37
+
 
 def replace_middle(input_list):
     """Replace all elements of the input list with the the values 42 and 37, in
     that order, except for the first two and last two elements.
     """
-    pass
+    input_list[2:-2] = [42, 37]
 
 def delete_third_and_seventh(input_list):
     """Remove the third and seventh elements of the input list."""
-    pass
+    del input_list[2:7:4]
 
 def delete_middle(input_list):
     """Remove all elements from the input list except for the first two and the
     last two.
     """
-    pass
+    del input_list[2:-2]
 
 """
 Part 2: Derived operations on lists
