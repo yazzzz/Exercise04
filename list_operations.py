366ef436d1f08996b221d3f37430dea67e9fb488
Hackbright Student <user@mini-002.(none)>
2014-02-06 18:03:15 -0800

    completed  part two of file to line 132

--- list_operations.py
+++ list_operations.py
@@ -101,26 +101,35 @@ the test_list_operations.py file for concrete examples of expected behavior.
 
 def custom_len(input_list):
     """custom_len(input_list) imitates len(input_list)"""
-    pass
+    count = 0
+    for i in input_list:
+        count = count +1
+    return count
+
 
 # For the next four functions, get clever using slice operations described in the first half
 def custom_append(input_list, value):
     """custom_append(input_list, value) imitates input_list.append(value)"""
-    pass
+    input_list[custom_len(input_list):] = [value]
 
 def custom_extend(input_list, values):
     """custom_extend(input_list, values) imitates input_list.extend(values)"""
-    pass
+    input_list[custom_len(input_list):] = values
 
 def custom_insert(input_list, index, value):
     """custom_insert(input_list, index, value) imitates
     input_list.insert(index, value)
     """
-    pass
+    input_list[index:index] = [value]
 
 def custom_remove(input_list, value):
     """custom_remove(input_list, value) imitates input_list.remove(value)"""
-    pass
+    # for i,v in enumerate(input_list):
+    #     myindex = 0
+    #     if i == value:
+    #         myindex = v    
+    # del input_list[myindex]
+    del input_list[input_list.index(value)]
 
 def custom_pop(input_list):
     """custom_pop(input_list) imitates input_list.pop()"""
